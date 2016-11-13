a9687afc2ca1050e28231f7eaf605a19a9d25c5e
Sebastian Proost <sebastian.proost@gmail.com>
2016-11-13 21:16:14 +0100

    cookies work again to show what is new

--- coagg/controllers.py
+++ coagg/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, make_response
+from flask import Blueprint, render_template, make_response, request
 
 from coagg.models import Comic
 
@@ -9,6 +9,11 @@ main = Blueprint('main', __name__)
 def index():
     images = Comic.query.all()
 
+    for i in images:
+        cookie_url = request.cookies.get(str(i.id))
+        if cookie_url != i.img_url:
+            i.new = True
+
     resp = make_response(render_template('main.html', images=images))
     return resp
 
@@ -18,6 +23,14 @@ def comic(cid):
     images = Comic.query.all()
     image = Comic.query.get_or_404(cid)
 
+    for i in images:
+        cookie_url = request.cookies.get(str(i.id))
+        if cookie_url != i.img_url and i.id != image.id:
+            i.new = True
+
     resp = make_response(render_template('main.html', images=images, image=image))
+
+    resp.set_cookie(str(cid), image.img_url)
+
     return resp
 
